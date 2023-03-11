9bc33800ea862884c83aa2335152dd3cdaeaa0b1
shahJainam961 <shahjainam961@gmail.com>
2023-03-11 15:21:57 +0530

    AFI-0001 Add functionality added

---
+++ src/main/java/org/app/Calculator.java
@@ -0,0 +1,27 @@
+package org.app;
+
+import java.util.Scanner;
+
+
+public class Calculator {
+
+    public int add(int a, int b){
+        return a+b;
+    }
+    public void runService() {
+        int num1, num2;
+        boolean flag = true;
+        char choice;
+        Scanner scan = new Scanner(System.in);
+        while(flag){
+            System.out.println("Enter your two numbers (space separated)");
+            num1 = scan.nextInt();
+            num2 = scan.nextInt();
+            System.out.println(add(num1,num2));
+            System.out.println("You wanna continue (Y/N) ? : ");
+            choice = scan.next().charAt(0);
+            if(choice=='Y') continue;
+            else flag = false;
+        }
+    }
+}
---
+++ src/main/java/org/app/Main.java
@@ -0,0 +1,7 @@
+package org.app;
+public class Main {
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        cal.runService();
+    }
+}
