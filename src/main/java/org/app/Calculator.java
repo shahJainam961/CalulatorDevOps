a3771a4bb591f65ddaa59282f386dae973f3b8a2
shahJainam961 <shahjainam961@gmail.com>
2023-03-12 23:03:33 +0530

    AFI-0011 Logging Added

--- src/main/java/org/app/Calculator.java
+++ src/main/java/org/app/Calculator.java
@@ -1,15 +1,19 @@
 package org.app;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.util.Scanner;
 
 
 public class Calculator {
 
+    private static final Logger log = LogManager.getLogger(Calculator.class);
     public int add(int a, int b){
-        return a-b;
+        return a+b;
     }
     public void runService() {
-        int num1, num2;
+
+        int num1, num2, num3;
         boolean flag = true;
         char choice;
         Scanner scan = new Scanner(System.in);
@@ -18,11 +22,15 @@ public class Calculator {
                 System.out.println("Enter your two integers(space separated)");
                 num1 = scan.nextInt();
                 num2 = scan.nextInt();
-                System.out.println(add(num1,num2));
+                num3 = add(num1,num2);
+                System.out.println(num3);
                 System.out.println("You wanna continue (Y/N) ? : ");
                 choice = scan.next().charAt(0);
-                if(choice=='Y') continue;
-                else flag = false;
+                log.info("|| Operation on "+num1+" and "+num2+" ==> "+num3+" ||");
+                if(choice!='Y'){
+                    flag = false;
+                    log.info("Exited...");
+                }
             }
             catch(Exception e){
                 System.out.println(e);
--- src/test/java/org/app/CalculatorTest.java
+++ src/test/java/org/app/CalculatorTest.java
@@ -9,7 +9,7 @@ public class CalculatorTest {
     Calculator cal = new Calculator();
     @Test
     public void add() {
-        Assert.assertEquals(-1 ,cal.add(5, 6));
-        Assert.assertEquals(0 ,cal.add(5, 5));
+        Assert.assertEquals(11 ,cal.add(5, 6));
+        Assert.assertEquals(10 ,cal.add(5, 5));
     }
 }
