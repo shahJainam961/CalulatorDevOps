2200f27a44f97c103e019bf663bce97495a2fc1d
shahJainam961 <shahjainam961@gmail.com>
2023-03-18 14:04:20 +0530

    AFI-0017 Thread sleep added for better visual

--- src/main/java/org/app/Main.java
+++ src/main/java/org/app/Main.java
@@ -1,6 +1,7 @@
 package org.app;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(10000);
         Calculator cal = new Calculator();
         cal.runService();
     }
